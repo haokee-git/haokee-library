670130d0b1c9dd37df8ab6156bbe8d69c710248b
haokee-git <19173155158@163.com>
2024-11-28 14:30:04 +0800

    update Doxygen

--- haokee/NumberTheory.kt
+++ haokee/NumberTheory.kt
@@ -4,6 +4,12 @@ import kotlin.math.sqrt
 
 class NumberTheory {
   companion object {
+    /**
+     * 判断一个数是否是素数。
+     *
+     * @param n 待判断的数。
+     * @return 如果是素数则返回 true，否则返回 false。
+     */
     @JvmStatic
     fun isPrime(n: Long): Boolean {
       if (n <= 1) {
@@ -17,6 +23,13 @@ class NumberTheory {
       return true
     }
 
+    /**
+     * 计算两个数的最大公约数。
+     *
+     * @param a 第一个数。
+     * @param b 第二个数。
+     * @return 两个数的最大公约数。
+     */
     @JvmStatic
     fun gcd(a: Long, b: Long): Long {
       var x = a
@@ -29,11 +42,24 @@ class NumberTheory {
       return x
     }
 
+    /**
+     * 计算两个数的最小公倍数。
+     *
+     * @param a 第一个数。
+     * @param b 第二个数。
+     * @return 两个数的最小公倍数。
+     */
     @JvmStatic
     fun lcm(a: Long, b: Long): Long {
       return a / gcd(a, b) * b
     }
 
+    /**
+     * 计算欧拉函数值。
+     *
+     * @param n 待计算的数。
+     * @return 欧拉函数值。
+     */
     @JvmStatic
     fun eulerTotient(n: Long): Long {
       var result = n
