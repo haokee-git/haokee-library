28beb828761df0ffcee3b5670dd9e225379be592
haokee-git <19173155158@163.com>
2024-11-29 11:33:15 +0800

    use [l, r) instead of [l, r]

--- haokee/Sort.kt
+++ haokee/Sort.kt
@@ -5,30 +5,33 @@ class Sort {
     private fun <T: Comparable<T>> merge(arr: Array<T>, left: Int, right: Int) {
       val mid = (left + right) / 2
       var i = left
-      var j = mid + 1
-      var k = left
+      var j = mid
       val temp = ArrayList<T>()
-      while (i <= mid && j <= right) {
-        temp.add(if (arr[i] < arr[j]) arr[i++] else arr[j++])
+      while (i < mid && j < right) {
+        temp.add(if (arr[i] <= arr[j]) {
+          arr[i++]
+        } else {
+          arr[j++]
+        })
       }
-      while (i <= mid) {
+      while (i < mid) {
         temp.add(arr[i++])
       }
-      while (j <= right) {
+      while (j < right) {
         temp.add(arr[j++])
       }
-      for (index in left..right) {
+      for (index in left..<right) {
         arr[index] = temp[index - left]
       }
     }
 
-    fun <T: Comparable<T>> mergeSort(arr: Array<T>, left: Int = 0, right: Int = arr.lastIndex) {
-      if (left >= right) {
+    fun <T: Comparable<T>> mergeSort(arr: Array<T>, left: Int = 0, right: Int = arr.size) {
+      if (left >= right - 1) {
         return
       }
       val mid = (left + right) / 2
       mergeSort(arr, left, mid)
-      mergeSort(arr, mid + 1, right)
+      mergeSort(arr, mid, right)
       merge(arr, left, right)
     }
   }
