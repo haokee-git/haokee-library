e5d60310b046776b9d6ea97afad0c88d41aaaa3a
haokee-git <19173155158@163.com>
2024-11-29 10:10:52 +0800

    fix bug in BinarySearch.kt

--- haokee/BinarySearch.kt
+++ haokee/BinarySearch.kt
@@ -2,9 +2,17 @@ package haokee
 
 class BinarySearch {
   companion object {
-    fun searchAnswer(range: IntRange, checker: (Int) -> Boolean): Int {
+    /**
+     * 区间二分查找。
+     *
+     * @param array 有序数组。
+     * @param target 目标值。
+     * @param checker 检查函数，`true` 时左移，`false` 时右移
+     * @return 目标值的下标，如果不存在则返回 `range.last + 1`。
+     */
+    fun searchInRange(range: IntRange, checker: (Int) -> Boolean): Int {
       var left = range.first
-      var right = range.last
+      var right = range.last + 1
       while (left < right) {
         val mid = left + (right - left) / 2
         if (checker(mid)) {
@@ -16,8 +24,26 @@ class BinarySearch {
       return left
     }
 
-    inline fun <reified T: Number> searchArray(array: Array<T>, target: T): Int {
-      return searchAnswer(array.indices) { Calculator.greaterEqual(array[it], target) }
+    /**
+     * 序列二分查找，寻找第一个大于等于 `target` 的元素
+     *
+     * @param array 有序数组。
+     * @param target 目标值。
+     * @return 目标值的下标，如果不存在则返回最后一个元素的后面一个元素下标。
+     */
+    inline fun <reified T: Comparable<T>> searchGreaterEqual(array: Array<T>, target: T): Int {
+      return searchInRange(array.indices) { array[it] >= target }
+    }
+
+    /**
+     * 序列二分查找，寻找第一个大于 `target` 的元素
+     *
+     * @param array 有序数组。
+     * @param target 目标值。
+     * @return 目标值的下标，如果不存在则返回最后一个元素的后面一个元素下标。
+     */
+    inline fun <reified T: Comparable<T>> searchGreater(array: Array<T>, target: T): Int {
+      return searchInRange(array.indices) { array[it] > target }
     }
   }
 }
